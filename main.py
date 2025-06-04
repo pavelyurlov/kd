f01d5791556bd7260491d42c3dd0588c83cb67f0
pavelyurlov <yurlov_pavel@mail.ru>
2025-06-04 16:03:57 +0300

    speed up inference

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import torch
 from trl import SFTTrainer, SFTConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+from teacher_pred import generate_text_from_samples
+
+
 # from peft import LoraConfig
 
 
@@ -20,13 +23,20 @@ def get_args():
     parser.add_argument("--model", type=str, default="qwen", choices=["qwen", "llama"])
     parser.add_argument("--data", type=str, default="real", choices=["dummy", "real"])
     parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--batch", type=int, default=512)
     args = parser.parse_args()
 
     model_name: str = args.model
     data_name: str = args.data
     n_epochs: int = args.epochs
+    batch_size: int = args.batch
 
-    return dict(model_name=model_name, data_name=data_name, n_epochs=n_epochs)
+    return dict(
+        model_name=model_name,
+        data_name=data_name,
+        n_epochs=n_epochs,
+        batch_size=batch_size,
+    )
 
 
 def dummy_data():
@@ -76,8 +86,8 @@ def saiga_data():
 
     dataset = dataset.map(rename_role)
 
-    train_dataset = dataset.filter(lambda _, idx: idx % 100 == 0, with_indices=True)
-    eval_dataset = dataset.filter(lambda _, idx: idx % 300 == 1, with_indices=True)
+    train_dataset = dataset.filter(lambda _, idx: idx % 10 == 0, with_indices=True)
+    eval_dataset = dataset.filter(lambda _, idx: idx % 30 == 1, with_indices=True)
 
     print(len(train_dataset), len(eval_dataset))
 
@@ -108,48 +118,18 @@ def model_names(name: str):
     return student_name, teacher_name
 
 
-def generate_text_from_sample(
+def generate_teacher_outputs(
     model: AutoModelForCausalLM,
     tokenizer: AutoTokenizer,
-    sample: dict,
-    max_new_tokens: int = 256,
-):
-    msgs = sample["messages"]
-
-    text_input = tokenizer.apply_chat_template(
-        msgs[:1], tokenize=False, add_generation_prompt=True
-    )
-    model_inputs = tokenizer(
-        text=[text_input],
-        return_tensors="pt",
-        padding=True,
-        truncation=True,
-        padding_side="left",
-    ).to(model.device)
-
-    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
-
-    trimmed_generated_ids = [
-        out_ids[len(in_ids) :]
-        for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-    output_text = tokenizer.batch_decode(
-        trimmed_generated_ids,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False,
-    )
-
-    return output_text[0]
-
-
-@torch.inference_mode
-def generate_teacher_outputs(
-    model: AutoModelForCausalLM, tokenizer: AutoTokenizer, dataset: Dataset
+    dataset: Dataset,
+    batch_size: int,
 ) -> Dataset:
     teacher_answers: list[str] = []
-    for sample in tqdm(dataset, desc="Teacher output generation"):
-        output = generate_text_from_sample(model, tokenizer, sample)
-        teacher_answers.append(output)
+    idx_range = range(0, len(dataset), batch_size)
+    for i in tqdm(idx_range, desc="Teacher output generation"):
+        samples = dataset[i : i + batch_size]
+        outputs = generate_text_from_samples(model, tokenizer, samples)
+        teacher_answers.extend(outputs)
 
     def replace_targets(sample: dict, idx: int):
         msgs = sample["messages"]
@@ -165,10 +145,13 @@ def generate_teacher_outputs(
 # def hard_response_distillation(
 #     model: AutoModelForCausalLM, tokenizer: AutoTokenizer, dataset: Dataset
 # ):
-#     pass
 
-
-def main(model_name: str = "qwen", data_name: str = "dummy", n_epochs: int = 1):
+def main(
+    model_name: str = "qwen",
+    data_name: str = "dummy",
+    n_epochs: int = 1,
+    batch_size: int = 512,
+):
     timestamp = datetime.now().strftime("%d.%m.%y_%H.%M.%S")
 
     student_name, teacher_name = model_names(model_name)
@@ -182,7 +165,9 @@ def main(model_name: str = "qwen", data_name: str = "dummy", n_epochs: int = 1):
         teacher_name, torch_dtype="bfloat16", device_map="auto"
     )
 
-    teacher_dataset = generate_teacher_outputs(teacher_model, tokenizer, train_dataset)
+    teacher_dataset = generate_teacher_outputs(
+        teacher_model, tokenizer, train_dataset, batch_size
+    )
 
     training_args = SFTConfig(
         # Save results
--- teacher_pred.py
+++ teacher_pred.py
@@ -4,94 +4,116 @@ from tqdm import tqdm
 
 import pandas as pd
 import json
+import torch
 
 
-model_name = "Qwen/Qwen2-0.5B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype="bfloat16",
-    device_map="auto",
-    # low_cpu_mem_usage=True,
-)
+BATCH_SIZE = 16
+STEP = 100
 
 
-dataset_name = "IlyaGusev/saiga_scored"
-dataset = load_dataset(dataset_name, split="train")
-dataset = dataset.filter(
-    lambda x: x["source"] == "gpt4"
-    and not x["is_bad_by_regex"]
-    and x["sonnet_complexity"] != "easy"
-    and x["language"] == "Russian"
-)
-
-
-def rename_role(sample: dict, add_system_message: bool = False):
-    msgs = sample["messages"]
-    for msg in msgs:
-        if msg["role"] == "bot":
-            msg["role"] = "assistant"
-    return sample
-
-
-dataset = dataset.map(rename_role)
-
-idxs = [100 * i for i in range(len(dataset) // 100 + 1)]
-miniset = dataset.select(idxs)
-
-print(len(dataset), len(miniset))
-
-
-def generate_text_from_sample(
+@torch.inference_mode
+def generate_text_from_samples(
     model: AutoModelForCausalLM,
     tokenizer: AutoTokenizer,
-    sample: dict,
+    samples: dict,
     max_new_tokens: int = 256,
 ):
-    msgs = sample["messages"]
+    msgs_batch = samples["messages"]
 
-    text_input = tokenizer.apply_chat_template(
-        msgs[:1], tokenize=False, add_generation_prompt=True
-    )
+    text_inputs = [
+        tokenizer.apply_chat_template(
+            msgs[:1], tokenize=False, add_generation_prompt=True
+        )
+        for msgs in msgs_batch
+    ]
 
     model_inputs = tokenizer(
-        text=[text_input],
+        text=text_inputs,
         return_tensors="pt",
-        # padding=True,
-        # truncation=True,
-        # padding_side="left",
+        padding=True,
+        truncation=True,
+        padding_side="left",
     ).to(model.device)
 
-    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=max_new_tokens,
+        # do_sample=False,
+        # num_beams=1,
+    )
 
     trimmed_generated_ids = [
         out_ids[len(in_ids) :]
         for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
     ]
 
-    output_text = tokenizer.batch_decode(
+    output_texts = tokenizer.batch_decode(
         trimmed_generated_ids,
         skip_special_tokens=True,
         clean_up_tokenization_spaces=False,
     )
 
-    return output_text[0]
+    return output_texts
 
 
-print(model.device)
-answers: list[str] = []
-# summary = []
-for sample in tqdm(miniset):
-    output = generate_text_from_sample(model, tokenizer, sample)
-    answers.append(output)
-    # summary.append(
-    #     {
-    #         "question": sample["messages"][0]["content"],
-    #         "target": sample["messages"][1]["content"],
-    #         "answer": output,
-    #     }
-    # )
+def main():
+    model_name = "Qwen/Qwen2-0.5B-Instruct"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype="bfloat16",
+        device_map="auto",
+        # low_cpu_mem_usage=True,
+    )
+
+    dataset_name = "IlyaGusev/saiga_scored"
+    dataset = load_dataset(dataset_name, split="train")
+    dataset = dataset.filter(
+        lambda x: x["source"] == "gpt4"
+        and not x["is_bad_by_regex"]
+        and x["sonnet_complexity"] != "easy"
+        and x["language"] == "Russian"
+    )
+
+    def rename_role(sample: dict, add_system_message: bool = False):
+        msgs = sample["messages"]
+        for msg in msgs:
+            if msg["role"] == "bot":
+                msg["role"] = "assistant"
+        return sample
+
+    dataset = dataset.map(rename_role)
+
+    idxs = [STEP * i for i in range(len(dataset) // STEP)]
+    miniset = dataset.select(idxs)
+
+    print(len(dataset), len(miniset))
+
+    print(model.device)
+    answers: list[str] = []
+    summary = []
+    for i in tqdm(
+        range(0, len(miniset), BATCH_SIZE), desc=f"{model_name} {BATCH_SIZE}"
+    ):
+        samples = miniset[i : i + BATCH_SIZE]
+        outputs = generate_text_from_samples(model, tokenizer, samples)
+        answers.extend(outputs)
+        for j in range(len(outputs)):
+            summary.append(
+                {
+                    "question": samples["messages"][j][0]["content"],
+                    "target": samples["messages"][j][1]["content"],
+                    "answer": outputs[j],
+                }
+            )
+
+    model_name = model_name.replace(":", "_")
+    if "/" in model_name:
+        model_name = model_name.split("/")[-1]
+    save_name = f"summary_{model_name}_batch{BATCH_SIZE}_step{STEP}.json"
+    with open(save_name, mode="w", encoding="utf-8") as f:
+        json.dump(summary, f, ensure_ascii=False, indent=4)
 
 
-# with open("summary.json", mode="w", encoding="utf-8") as f:
-#     json.dump(summary, f, ensure_ascii=False, indent=4)
+if __name__ == "__main__":
+    main()
